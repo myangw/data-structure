7d8ff409db50b9e4cf2956b4fca1cb0fb975eda2
me0768 <kmh4503@gmail.com>
2018-06-17 15:31:03 +0900

    postfix, postfixCalculator (using stack) refactored

--- stack and queue/pCal.c
+++ stack and queue/pCal.c
@@ -1,19 +1,19 @@
+/* The calculator which gets postfix-formed input */
 #include <stdio.h>
 #include <stdlib.h>
 
-/* stack */
 struct stack_t{
     char *key;
     int top;
     int capacity;
 };
-//Stack Function
+
 struct stack_t *CreateStack(int capacity){
     struct stack_t *new = (struct stack_t *)malloc(sizeof(struct stack_t));
 
     new->capacity = capacity;
     new->top = -1;
-    new->key = (char *)malloc(sizeof(char)* new->capacity);
+    new->key = (char *)malloc(sizeof(char) * new->capacity);
 
     return new;
 }
@@ -26,21 +26,25 @@ int IsFull(struct stack_t *stack){
 int IsEmpty(struct stack_t *stack){
     return stack->top == -1;
 }
+
 void Push(struct stack_t *stack, int element){
     if(!IsFull(stack)){
         stack->key[++stack->top] = element;
     }
 }
+
 int Pop(struct stack_t *stack){
     if(!IsEmpty(stack)){
         return stack->key[stack->top--];
     }
 }
+
 int Top(struct stack_t *stack){
     if(!IsEmpty(stack)){
         return stack->key[stack->top];
     }
 }
+
 void PrintStack(struct stack_t *stack){
     if(!IsEmpty(stack)){
         for(int i=0;i<stack->top;i++){
@@ -57,12 +61,6 @@ int CalTwo(struct stack_t *stack, char *letter, int *result){
     int operand1 = Pop(stack);
     int operand2 = Pop(stack);
 
-   // if(operand2 > operand1){
-      //  int tmp = operand2;
-       // operand2 = operand1;
-       // operand1 = tmp;
-    //}
-
     switch(*letter){
         case '+': return operand2 + operand1; break;
         case '-': return operand2 - operand1; break;
@@ -71,19 +69,21 @@ int CalTwo(struct stack_t *stack, char *letter, int *result){
         case '%': return operand2 % operand1; break;
     }
 }
+
 int CharToInt(char *letter){
     return *letter -48;
 }
+
 void Calculate(struct stack_t *stack, char *letter){
     int result = 0;
-    if(*letter>48 && *letter<58){
+    if(*letter > 48 && *letter < 58){
         Push(stack, CharToInt(letter));
     }else{
         result = CalTwo(stack,letter,&result);
         Push(stack, result);
     }
 }
-/* Conversion from infix to postfix */
+
 int main(int argc, const char *argv[])
 {
     if(argc ==2){
--- stack and queue/postfix.c
+++ stack and queue/postfix.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* stack */
 struct stack_t{
     char *key;
     int top;
     int capacity;
 };
-//Stack Function
+
+/* stack functions */
+
 struct stack_t *CreateStack(int capacity){
     struct stack_t *new = (struct stack_t *)malloc(sizeof(struct stack_t));
 
     new->capacity = capacity;
     new->top = -1;
-    new->key = (char *)malloc(sizeof(char)* new->capacity);
+    new->key = (char *)malloc(sizeof(char) * new->capacity);
 
     return new;
 }
@@ -26,33 +27,39 @@ int IsFull(struct stack_t *stack){
 int IsEmpty(struct stack_t *stack){
     return stack->top == -1;
 }
+
 void Push(struct stack_t *stack, char element){
     if(!IsFull(stack)){
         stack->key[++stack->top] = element;
     }
 }
+
 char Pop(struct stack_t *stack){
     if(!IsEmpty(stack)){
         return stack->key[stack->top--];
     }
 }
+
 char Top(struct stack_t *stack){
     if(!IsEmpty(stack)){
         return stack->key[stack->top];
     }
 }
+
 void PrintStack(struct stack_t *stack){
     if(!IsEmpty(stack)){
-        for(int i=0;i<stack->top;i++){
+        for(int i = 0; i < stack->top; i++){
             printf("%d", stack->key[i]);
         }
     }
 }
+
 void FreeStack(struct stack_t *stack){
     free(stack->key);
     free(stack);
 }
-/* Conversion from infix to postfix */
+
+/* Functions for conversion to postfix */
 
 void Printout(char *converted){
     printf("\nconverted postfix form:");
@@ -63,7 +70,7 @@ void Printout(char *converted){
 }
 
 int CharToInt(char *letter){
-    return *letter -48;
+    return *letter - 48;
 } 
 int Priority(char *operator){
     switch(*operator){
@@ -78,54 +85,41 @@ int Priority(char *operator){
     }
 }
 
-//put it to a stack or a variable to print. 
 void ConvertToPostfix(char *letter, char *output, int *count, struct stack_t *stack){
-    //if it is numbers, just put to the variable 'output'
-    if(*letter>48 && *letter<58){
+    //operands ==> output array
+    if(*letter > 48 && *letter < 58){
         output[*count] = *letter;
     }else{
-        //operators==>stack
+        //operators ==> stack
         char top = Top(stack);
-        if(IsEmpty(stack) || Priority(letter)>Priority(&top) || top=='('){
+        if(IsEmpty(stack) || Priority(letter) > Priority(&top) || top == '('){
             Push(stack, *letter);
         }else{
-            if(*letter == ')'){
-                while(!IsEmpty(stack) && Priority(letter)<=Priority(&top)){
-                    if(Top(stack)=='('){
-                        Pop(stack);
-                        break;
-                    }   
-                }   
-                }else{    
-                while(!IsEmpty(stack) && Priority(letter)<=Priority(&top) ){
-                    if(Top(stack)=='('||Top(stack)==')'){
-                        Pop(stack);
-                    }else{
-                        output[*count] = Pop(stack);
-                        *count = *count + 1;
-                    }
-                }  
-                Push(stack, *letter);
-            }
+            while(!IsEmpty(stack) && Priority(letter) <= Priority(&top)){
+                if(Top(stack) == '(' || Top(stack) == ')'){
+                    Pop(stack);
+                }else{
+                    output[*count] = Pop(stack);
+                    *count = *count + 1;
+                }
+            }  
+            Push(stack, *letter);
         } 
     }
 }
 
 int main(int argc, const char *argv[])
 {
-    if(argc ==2){
+    if(argc == 2){
         FILE *fp = fopen(argv[1], "r");
         char buf;
-        char *output = (char *)malloc(sizeof(char)*64);
-        int count =0;
+        char *output = (char *)malloc(sizeof(char) * 64);
+        int count = 0;
         struct stack_t *stack = CreateStack(10);
         printf("Original infix form:");
         if(fp){
-            while(1){
+            while(buf != '#'){
                 buf = fgetc(fp);
-                if(buf=='#'){
-                    break;
-                }
                 printf("%c",buf);
                 ConvertToPostfix(&buf,output,&count,stack);
                 count++;
